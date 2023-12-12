491bd5883aabe901a1f93e9c68d0a87a9981bb7b
kazumakurotani <kazumakurotani.1115@gmail.com>
2023-12-13 01:49:49 +0900

    途中経過_datasetを整理した

--- app/button_handlers/button_handler_home_window.py
+++ app/button_handlers/button_handler_home_window.py
@@ -2,8 +2,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 import setup_window
 import sys
 
-def openWindow_imageAnalysis(HomeWindow):
-
+def open_window_image_analysis(HomeWindow):
     # ウィンドウを開く
     setup_window.imageAnalysisWindow = setup_window.ImageAnalysisWindow()
     setup_window.imageAnalysisWindow.show()
--- app/setup_window.py
+++ app/setup_window.py
@@ -1,10 +1,12 @@
-from PyQt5 import QtCore, QtWidgets
+from PyQt5 import QtWidgets
 import sys
 
 # layout
-from layout import layout_home_window
+from layouts_window import layout_home_window
+from layouts_window import layout_image_analysis_window
 
 # button handler
+from button_handlers import button_handler_home_window
 
 
 class HomeWindow(QtWidgets.QMainWindow, layout_home_window.Ui_home_window):
@@ -18,11 +20,24 @@ class HomeWindow(QtWidgets.QMainWindow, layout_home_window.Ui_home_window):
     def __init__(self):
         super(HomeWindow, self).__init__()
         self.setupUi(self)
-        # self.connect_handler_to_main_window_button()
+        self.connect_handler_to_main_window_button()
 
     def connect_handler_to_main_window_button(self):
         """ボタンと処置関数を接続"""
-        self.button_openImageAnalysisWindow.clicked.connect(lambda: button_handler_main_window.openWindow_imageAnalysis(self))
+        self.pushButton_for_FeatureAnalysis.clicked.connect(lambda: button_handler_home_window.open_window_image_analysis(self))
+
+
+class ImageAnalysisWindow(QtWidgets.QMainWindow, layout_image_analysis_window.Ui_MainWindow_for_ImageAnalysis):
+    """
+    Manage the image analysis window.
+
+    Args:
+        QtWidgets.QMainWindow (class): Qt Library
+        layout_home_window.Ui_home_window (class): layout for homewindow
+    """
+    def __init__(self):
+        super(ImageAnalysisWindow, self).__init__()
+        self.setupUi(self)
 
 
 if __name__ == "__main__":
